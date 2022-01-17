a07e274faa0cdb22f27f0aa7396cfe2c0c46b647
Tony Bowden <tony@tmtm.com>
2022-01-17 15:59:09 +0200

    compare enwiki COGS

---
+++ sources/cogs-enwiki/scraper.rb
@@ -0,0 +1,34 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no portrait name start end].freeze
+    end
+
+    def name_cell
+      super.css('b')
+    end
+
+    def tds
+      noko.css('th,td')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
